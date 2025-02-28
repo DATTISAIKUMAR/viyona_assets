5ae05329f69796d94a38c39675db7df8c3e4db06
PranayLambu <pranay2001kulkarni@gmail.com>
2025-02-28 18:00:34 +0530

    modified login

--- appservices/superAdmin/controllers/login.py
+++ appservices/superAdmin/controllers/login.py
@@ -5,23 +5,25 @@ from controllers.util import *
 statuses=['Inactive','Active']
 router = APIRouter()
 
-
-
 def error_function(request):
-    users=Logfiles_data.objects.order_by("-id")
-    page = 1
-    per_page = 10
-    start = (page - 1) * per_page
-    end = start + per_page
-    total_pages = ceil(len(users) / per_page)
-    paginated_data = users[start:end]
-    content= {
-        "start":start,
-        "total_pages": total_pages,
-        "request":request,
-        'data':paginated_data
-    }
-    return templates.TemplateResponse('loginfiles.html',content)
+    try:
+        users=Logfiles_data.objects.order_by("-id")
+        page = 1
+        per_page = 10
+        start = (page - 1) * per_page
+        end = start + per_page
+        total_pages = ceil(len(users) / per_page)
+        paginated_data = users[start:end]
+        content= {
+            "start":start,
+            "total_pages": total_pages,
+            "request":request,
+            'data':paginated_data
+        }
+        return templates.TemplateResponse('loginfiles.html',content)
+    except Exception as e:
+        message="exception has occured.."
+        return templates.TemplateResponse('loginfiles.html', {'request': request, 'message': message})
 
 
 @router.get('/login',response_class=HTMLResponse)
@@ -36,78 +38,94 @@ async def signup_page(request: Request):
 
 @router.post('/login1',response_class=HTMLResponse)
 async def login_page(request:Request):
-    form_data=await request.form()
-    email=form_data.get('email')
-    password=form_data.get('password')
-    date=datetime.now()
-    data=Signup.objects(email=email,password=password).first()
-    if not data or ('@' not in email[2:-5] and '.' not in email[-5::]):
-        message="Invalid Password or Email..."
-        return templates.TemplateResponse('login.html',{'request':request,'message':message})
-    Logfiles_data(loginid=data.id,date=date).save()    
-    name_empty=Managedata.objects(name='')
-    if name_empty:
-        name_empty.update(status="Unassigned")
-    total_systems=Managedata.objects()
-    desktop_total=Desktopdata.objects()
-  
-    content = {
-        'request': request,
-        'total_systems': len(total_systems),
-         'desktop_total':len(desktop_total)
-        
-    }
-    return templates.TemplateResponse('asserts.html',content)
+    try:
+        form_data=await request.form()
+        email=form_data.get('email')
+        password=form_data.get('password')
+        date=datetime.now()
+        data=Signup.objects(email=email,password=password).first()
+        if not data or ('@' not in email[2:-5] and '.' not in email[-5::]):
+            message="Invalid Password or Email..."
+            return templates.TemplateResponse('login.html',{'request':request,'message':message})
+        Logfiles_data(loginid=data.id,date=date).save()    
+        name_empty=Managedata.objects(name='')
+        if name_empty:
+            name_empty.update(status="Unassigned")
+        total_systems=Managedata.objects()
+        desktop_total=Desktopdata.objects()
+    
+        content = {
+            'request': request,
+            'total_systems': len(total_systems),
+            'desktop_total':len(desktop_total)
+            
+        }
+        return templates.TemplateResponse('asserts.html',content)
+    except Exception as e:
+        message="exception has occured.."
+        return templates.TemplateResponse('asserts.html', {'request': request, 'message': message})
 
 
 @router.post('/signup',response_class=HTMLResponse)
 async def signup(request: Request):
-    form_data = await request.form()
-    name = form_data.get('name')
-    email = form_data.get('email')
-    password = form_data.get('password')
-    password1 = form_data.get('password1')
-    role = form_data.get('role')
-    reason=form_data.get('reason')
-   
-    date= datetime.now()
-    required_fields=[name,email,password,password1,role,reason]
+    try:
+        form_data = await request.form()
+        name = form_data.get('name')
+        email = form_data.get('email')
+        password = form_data.get('password')
+        password1 = form_data.get('password1')
+        role = form_data.get('role')
+        reason=form_data.get('reason')
+    
+        date= datetime.now()
+        required_fields=[name,email,password,password1,role,reason]
 
-    if password != password1:
-        message = "Passwords do not match!"
-        return templates.TemplateResponse('logfile.html', {'request': request, 'message': message})
-    if('@' not in email[2:-5] and '.' not in email[-5::]):
-        message='Invalid email.....'
-        return templates.TemplateResponse('logfile.html', {'request': request, 'message': message})
-    data=Signup.objects(email=email, password=password).first()
-    if(data):
-        message="user already exits.."
-        return templates.TemplateResponse('logfile.html', {'request': request, 'message': message})
-    if(all(required_fields)):
-        Signup(name=name, email=email, password=password, role=role,reason=reason,Date=date).save()
-    else:
-        message="Please enter valid fields.."
-        return templates.TemplateResponse('logfile.html', {'request': request, 'message': message})
+        if password != password1:
+            message = "Passwords do not match!"
+            return templates.TemplateResponse('logfile.html', {'request': request, 'message': message})
+        if('@' not in email[2:-5] and '.' not in email[-5::]):
+            message='Invalid email.....'
+            return templates.TemplateResponse('logfile.html', {'request': request, 'message': message})
+        data=Signup.objects(email=email, password=password).first()
+        if(data):
+            message="user already exits.."
+            return templates.TemplateResponse('logfile.html', {'request': request, 'message': message})
+        if(all(required_fields)):
+            Signup(name=name, email=email, password=password, role=role,reason=reason,Date=date).save()
+        else:
+            message="Please enter valid fields.."
+            return templates.TemplateResponse('logfile.html', {'request': request, 'message': message})
 
-    return RedirectResponse('/logfile1', status_code=303)
+        return RedirectResponse('/logfile1', status_code=303)
+    except Exception as e:
+        message="exception has occured.."
+        return templates.TemplateResponse('logfile.html', {'request': request, 'message': message})
 
 @router.get('/logfile1')
 async def adminfiles(request:Request):
-    admins=Signup.objects()
-    return templates.TemplateResponse('logfile.html',{'request':request,"data":admins})
+    try:
+        admins=Signup.objects()
+        return templates.TemplateResponse('logfile.html',{'request':request,"data":admins})
+    except Exception as e:
+        message="exception has occured.."
+        return templates.TemplateResponse('logfile.html', {'request': request, 'message': message})
 
 
 @router.post('/logfile')
 async def logfile(request:Request):
-    form_data=await request.form()
-    email=form_data.get('email')
-    password=form_data.get('password')
-    data=Super_admin.objects(email=email,password=password).first()
-    if not data:
-        return templates.TemplateResponse('super_admin.html',{'request':request,'message':"Invalid details data..."})
-    
-    admins=Signup.objects()
-    return templates.TemplateResponse('logfile.html',{'request':request,'data':admins})
+    try:
+        form_data=await request.form()
+        email=form_data.get('email')
+        password=form_data.get('password')
+        data=Super_admin.objects(email=email,password=password).first()
+        if not data:
+            return templates.TemplateResponse('super_admin.html',{'request':request,'message':"Invalid details data..."})
+        
+        admins=Signup.objects()
+        return templates.TemplateResponse('logfile.html',{'request':request,'data':admins})
+    except Exception as e:
+        message="exception has occured.."
+        return templates.TemplateResponse('logfile.html', {'request': request, 'message': message})
 
 @router.get('/super_admin')
 async def super_admin(request:Request):
@@ -117,10 +135,14 @@ async def super_admin(request:Request):
 
 @router.post('/super_admin_signup')
 async def super_admin_sinup(email:str,password:str,request:Request):
-    if('@' not in email[2:-3]) and ('.' not in email[-4::]):
-        return 1
-    Super_admin(email=email,password=password).save()
-    return 0
+    try:
+        if('@' not in email[2:-3]) and ('.' not in email[-4::]):
+            return 1
+        Super_admin(email=email,password=password).save()
+        return templates.TemplateResponse('super_admin.html', {'request': request, 'message': message})
+    except Exception as e:
+        message="exception has occured.."
+        return templates.TemplateResponse('super_admin.html', {'request': request, 'message': message})
 
 
 
@@ -133,38 +155,48 @@ async def login_detail_page(request:Request):
 
 @router.get('/delete/{id}')
 async def delete_page(id,request:Request):
-    Logfiles_data.objects(loginid=id).delete()
-    Signup.objects(id=id).delete()
-    data=Signup.objects()
-    return templates.TemplateResponse('logfile.html',{'request':request,'data':data})
+    try:
+        Logfiles_data.objects(loginid=id).delete()
+        Signup.objects(id=id).delete()
+        data=Signup.objects()
+        return templates.TemplateResponse('logfile.html',{'request':request,'data':data})
+    except Exception as e:
+        message="exception has occured.."
+        return templates.TemplateResponse('logfile.html', {'request': request, 'message': message})
+
     
 
 @router.get('/asserts_dashboard')
 async def dashboard(request:Request):
-    total_systems=Managedata.objects()
-    desktop_total=Desktopdata.objects()
-    other_systems=other_systemsdata.objects()
-
-    
-    return templates.TemplateResponse('asserts.html',{'request': request,'total_systems': len(total_systems),'desktop_total':len(desktop_total),'other_systems_total':len(other_systems)})
-
+    try:
+        total_systems=Managedata.objects()
+        desktop_total=Desktopdata.objects()
+        other_systems=other_systemsdata.objects()
+        return templates.TemplateResponse('asserts.html',{'request': request,'total_systems': len(total_systems),'desktop_total':len(desktop_total),'other_systems_total':len(other_systems)})
+    except Exception as e:
+        message="exception has occured.."
+        return templates.TemplateResponse('asserts.html', {'request': request, 'message': message})
 
 
 
 
 @router.get('/pagination/{page_num}',response_class=HTMLResponse)
 async def pagination(page_num:int,request:Request):
-    user=Logfiles_data.objects.order_by("-id")
-    page = page_num
-    per_page = 10
-    start = (page - 1) * per_page
-    end = start + per_page
-    total_pages = ceil(len(user) / per_page)
-    paginated_data = user[start:end]
-    content= {
-        "start":start,
-        "total_pages": total_pages,
-        "request":request,
-        'data':paginated_data
-    }
-    return templates.TemplateResponse('loginfiles.html',content)
+    try:
+        user=Logfiles_data.objects.order_by("-id")
+        page = page_num
+        per_page = 10
+        start = (page - 1) * per_page
+        end = start + per_page
+        total_pages = ceil(len(user) / per_page)
+        paginated_data = user[start:end]
+        content= {
+            "start":start,
+            "total_pages": total_pages,
+            "request":request,
+            'data':paginated_data
+        }
+        return templates.TemplateResponse('loginfiles.html',content)
+    except Exception as e:
+        message="exception has occured.."
+        return templates.TemplateResponse('loginfiles.html', {'request': request, 'message': message})
