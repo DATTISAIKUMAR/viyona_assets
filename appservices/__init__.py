cde4c81991e8912086a4d1587af140ff186acde4
PranayLambu <pranay2001kulkarni@gmail.com>
2025-02-28 17:29:08 +0530

    added try and catch to laptops.py and changed router

--- appservices/__init__.py
+++ appservices/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from superAdmin.controllers.login import router
-from superAdmin.controllers.laptops import dashboard
+from superAdmin.controllers.laptops import laptops
 from superAdmin.controllers.desktop import desktop
 from superAdmin.controllers.otherSystems import other_systems
 
@@ -9,7 +9,7 @@ app=FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(router,tags=["login"])
-app.include_router(dashboard,tags=["managing system data"])
+app.include_router(laptops,tags=["managing system data"])
 app.include_router(desktop,tags=["desktop dashboard data"])
 app.include_router(other_systems,tags=["other systems dashboard.."])
 
